23ec21290c10fb4d84a82afde00cf0f4f90e8f50
Diegos Lores <diegoslores@gmail.com>
2019-09-09 22:01:59 +0200

    sample medicines

--- src/sample-medicine.js
+++ src/sample-medicine.js
@@ -1,18 +1,16 @@
 // This is just some sample data so you don't have to think of your own!
 const pills = {
   medicine1: {
-    name: "Pacific Halibut",
-    image: "/images/hali.jpg",
-    desc:
-      "Everyones favorite white fish. We will cut it to the size you need and ship it.",
+    name: "Betadine",
+    image: "/images/betadine.jpg",
+    desc: "Liquido utilizado para la desinfeccion de heridas.",
     price: 1724
   },
 
   medicine2: {
-    name: "Lobster",
-    image: "/images/lobster.jpg",
-    desc:
-      "These tender, mouth-watering beauties are a fantastic hit at any dinner party.",
+    name: "Paracetamol",
+    image: "/images/paracetamol.jpg",
+    desc: "Comprimidos recubiertos para el tratamiento de la fiebre.",
     price: 3200
   }
 };
