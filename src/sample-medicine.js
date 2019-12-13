9f1601b0b36b76191f806399974adf3d6f18c187
Diegosl <44493933+diegoslores@users.noreply.github.com>
2019-12-13 10:21:42 +0100

    Update sample-medicines with new data

--- src/sample-medicine.js
+++ src/sample-medicine.js
@@ -1,17 +1,74 @@
-// This is just some sample data so you don't have to think of your own!
 const pills = {
   medicine1: {
-    name: "Betadine",
-    image: "/images/betadine.jpg",
+    caducidad: "05/03/2021",
+    cantidad: "250",
+    cantidadEnvase: "500 ml",
+    composicion: "10%. povidona yodada",
     desc: "Liquido utilizado para la desinfeccion de heridas.",
-    price: 1724
+    fabricante: "Meda",
+    image: "/images/betadine.jpg",
+    name: "Betadine",
+    pacientes: "Pedro Perez,\nMaría Soledad,\nArmando Casitas",
+    prescripcion: "Aplicar en la herida con una gasa",
+    tipo: "pomada",
+    envasesTotales: "1"
   },
 
   medicine2: {
-    name: "Paracetamol",
-    image: "/images/paracetamol.jpg",
+    caducidad: "19/12/2019",
+    cantidad: "5",
+    cantidadEnvase: "50 comprimidos",
+    composicion: "500 mg",
     desc: "Comprimidos recubiertos para el tratamiento de la fiebre.",
-    price: 3200
+    fabricante: "Normon",
+    image: "/images/paracetamol.jpg",
+    name: "Paracetamol",
+    pacientes: "Pedro Perez,\nMaría Soledad,\nArmando Casitas",
+    prescripcion: "1 comprimido con cada comida",
+    tipo: "comprimido",
+    envasesTotales: "3"
+  },
+  medicine3: {
+    caducidad: "20/01/2022",
+    cantidad: "50",
+    cantidadEnvase: "60 g",
+    composicion: "5 mg/g. Pentosano polisulfato sódico",
+    desc: "Pomada específica para las varices, los hematomas y los golpes. Mejoran la circulación.",
+    fabricante: "Lacer",
+    image: "/images/trombocid.jpg",
+    name: "Thrombocid",
+    pacientes: "Pedro Perez,\nMaría Soledad,\nArmando Casitas",
+    prescripcion: "aplicar sobre la zona 2 veces al día",
+    tipo: "pomada",
+    envasesTotales: "1"
+  },
+  medicine4: {
+    caducidad: "12/04/2020",
+    cantidad: "6",
+    cantidadEnvase: "20",
+    composicion: "",
+    desc: "Tiritas para cubrir heridas superficiales",
+    fabricante: "",
+    image: "/images/tiritas.jpg",
+    name: "otros",
+    pacientes: "Pedro Perez,\nMaría Soledad",
+    prescripcion: "Una con cada herida",
+    tipo: "otros",
+    envasesTotales: "2"
+  },
+  medicine5: {
+    caducidad: "20/01/2021",
+    cantidad: "18",
+    cantidadEnvase: "30 comprimidos",
+    composicion: "650 miligramos",
+    desc: "Para la inflamación",
+    fabricante: "Normon",
+    image: "https://static.laverdad.es/www/multimedia/20181...",
+    name: "Ibuprofeno",
+    pacientes: "Pedro Perez,\nMaría Soledad,\nArmando Casitas",
+    prescripcion: "1 comprimido cada 8 horas",
+    tipo: "comprimido",
+    envasesTotales: "5"
   }
 };
 
