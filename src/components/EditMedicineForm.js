720b845d79c4cd716a8e634ffdfbf332279d6a56
Diegos Lores <diegoslores@gmail.com>
2019-12-15 14:23:09 +0100

    update css

--- src/components/EditMedicineForm.js
+++ src/components/EditMedicineForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Form, Col } from "react-bootstrap";
+import { Button, Form, Col, Row } from "react-bootstrap";
 
 class EditMedicineForm extends React.Component {
   handleChange = event => {
@@ -20,108 +20,116 @@ class EditMedicineForm extends React.Component {
 
   render() {
     return (
-      <form className="medicine-edit">
-        <Form.Row>
-          <Col>
-            <a id={this.props.medicine.name}/>
-            <Form.Control
-              type="text"
-              name="name"
-              onChange={this.handleChange}
-              value={this.props.medicine.name}
-            />
+      <Row clasName="inventario">
+        <Col sm="2">
+          <h2>Inventario</h2>
+        </Col>
+        <Col sm="10">
+          <Form className="medicine-edit row">
+            <Col>
+              <a id={this.props.medicine.name} />
+              <Form.Control
+                type="text"
+                name="name"
+                onChange={this.handleChange}
+                value={this.props.medicine.name}
+              />
 
-            <Form.Control
-              as="textarea"
-              type="text"
-              name="desc"
-              onChange={this.handleChange}
-              value={this.props.medicine.desc}
-            />
+              <Form.Control
+                as="textarea"
+                type="text"
+                name="desc"
+                onChange={this.handleChange}
+                value={this.props.medicine.desc}
+              />
 
-            <select
-              name="tipo"
-              onChange={this.handleChange}
-              value={this.props.medicine.tipo}
-            >
-              <option value="comprimido">Comprimido</option>
-              <option value="pomada">Pomada</option>
-              <option value="liquido">Liquido</option>
-              <option value="otro">Otro</option>
-            </select>
+              <select
+                name="tipo"
+                onChange={this.handleChange}
+                value={this.props.medicine.tipo}
+              >
+                <option value="comprimido">Comprimido</option>
+                <option value="pomada">Pomada</option>
+                <option value="liquido">Liquido</option>
+                <option value="otro">Otro</option>
+              </select>
 
-            <Form.Control
-              type="text"
-              name="composicion"
-              onChange={this.handleChange}
-              value={this.props.medicine.composicion}
-            />
+              <Form.Control
+                type="text"
+                name="composicion"
+                onChange={this.handleChange}
+                value={this.props.medicine.composicion}
+              />
 
-            <Form.Control
-              type="text"
-              name="cantidad"
-              onChange={this.handleChange}
-              value={this.props.medicine.cantidad}
-            />
+              <Form.Control
+                type="text"
+                name="cantidad"
+                onChange={this.handleChange}
+                value={this.props.medicine.cantidad}
+              />
 
-            <Form.Control
-              type="text"
-              name="cantidad_envase"
-              onChange={this.handleChange}
-              value={this.props.medicine.cantidadEnvase}
-            />
+              <Form.Control
+                type="text"
+                name="cantidad_envase"
+                onChange={this.handleChange}
+                value={this.props.medicine.cantidadEnvase}
+              />
 
-            <Form.Control
-              type="text"
-              name="prescripcion"
-              onChange={this.handleChange}
-              value={this.props.medicine.prescripcion}
-            />
+              <Form.Control
+                type="text"
+                name="prescripcion"
+                onChange={this.handleChange}
+                value={this.props.medicine.prescripcion}
+              />
 
-            <Form.Control
-              type="text"
-              name="caducidad"
-              onChange={this.handleChange}
-              value={this.props.medicine.caducidad}
-            />
+              <Form.Control
+                type="text"
+                name="caducidad"
+                onChange={this.handleChange}
+                value={this.props.medicine.caducidad}
+              />
 
-            <Form.Control
-              type="text"
-              name="uds_totales"
-              onChange={this.handleChange}
-              value={this.props.medicine.envasesTotales}
-            />
+              <Form.Control
+                type="text"
+                name="uds_totales"
+                onChange={this.handleChange}
+                value={this.props.medicine.envasesTotales}
+              />
 
-            <Form.Control
-              type="text"
-              name="fabricante"
-              onChange={this.handleChange}
-              value={this.props.medicine.fabricante}
-            />
-          </Col>
-          <Col>
-            <img src={this.props.medicine.image} alt={this.props.medicine.name} />
-            <Form.Control
-              type="text"
-              name="image"
-              onChange={this.handleChange}
-              value={this.props.medicine.image}
-            />
+              <Form.Control
+                type="text"
+                name="fabricante"
+                onChange={this.handleChange}
+                value={this.props.medicine.fabricante}
+              />
+            </Col>
+            <Col>
+              <img
+                src={this.props.medicine.image}
+                alt={this.props.medicine.name}
+              />
+              <Form.Control
+                type="text"
+                name="image"
+                onChange={this.handleChange}
+                value={this.props.medicine.image}
+              />
 
-            <Form.Control
-              as="textarea"
-              type="text"
-              name="pacientes"
-              onChange={this.handleChange}
-              value={this.props.medicine.pacientes}
-            />
+              <Form.Control
+                as="textarea"
+                type="text"
+                name="pacientes"
+                onChange={this.handleChange}
+                value={this.props.medicine.pacientes}
+              />
 
-            <Button type="button" onClick={this.handleDelete}>
-              Borrar Medicina
-            </Button>
-          </Col>
-        </Form.Row>
-      </form>
+              <Button type="button" onClick={this.handleDelete}>
+                Borrar Medicina
+              </Button>
+            </Col>
+          </Form>
+        </Col>
+      </Row>
     );
   }
 }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,22 +1,20 @@
 import React from "react";
+
 import EditMedicineForm from "./EditMedicineForm";
 
 class Inventory extends React.Component {
   render() {
     return (
       <>
-        <div className="inventario">
-          <h2>Inventario</h2>
-          {Object.keys(this.props.medicine).map(pillKey => (
-            <EditMedicineForm
-              key={pillKey}
-              medicineKey={pillKey}
-              medicine={this.props.medicine[pillKey]}
-              updateMedicine={this.props.updateMedicine}
-              deleteMedicine={this.props.deleteMedicine}
-            />
-          ))}
-        </div>
+        {Object.keys(this.props.medicine).map(pillKey => (
+          <EditMedicineForm
+            key={pillKey}
+            medicineKey={pillKey}
+            medicine={this.props.medicine[pillKey]}
+            updateMedicine={this.props.updateMedicine}
+            deleteMedicine={this.props.deleteMedicine}
+          />
+        ))}
       </>
     );
   }
